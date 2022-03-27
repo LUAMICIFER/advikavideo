2aa03f12d0ad10fc12ba9a7c51b9b3f5dc00d4dd
LUAMICIFER <96227537+LUAMICIFER@users.noreply.github.com>
2022-03-27 18:08:39 +0530

    Update __init__.py

--- driver/database/__init__.py
+++ driver/database/__init__.py
@@ -1,4 +1,1 @@
-""" database copyright """
-
-# thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
-# implemented by levina-lab (https://github.com/levina-lab)
+thanks to luami cifer
